18feafd7734560e2d948ea957c509b5d07809b43
b1nhan <sukiakira1411@gmail.com>
2026-01-03 22:27:50 +0700

    test

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,13 +1,23 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import dotenv from 'dotenv'
+dotenv.config();
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
+
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import fs from 'fs';
+
 
 // 2. Initialize the Adapter
 const adapter = new PrismaMariaDb({
-  host: process.env.DB_HOST, // Use 127.0.0.1 to avoid IPv6 issues
-  port: parseInt(process.env.DB_PORT),
-  user: process.env.DB_USERNAME, // Check your .env variable names
+  host: process.env.DB_HOST || '127.0.0.1', // Use 127.0.0.1 to avoid IPv6 issues
+  port: parseInt (process.env.DB_PORT) || 3306,
+  user: process.env.DB_USERNAME, 
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE, // Fix: Previously was set to process.env.HOST
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync('./ca.pem'),
+    rejectUnauthorized: false,
+  },
   connectionLimit: 10,
 });
 
